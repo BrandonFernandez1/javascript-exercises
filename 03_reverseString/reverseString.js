f826da4e3d72545b9af2b2f56847ecfbcb37d90c
Brandon Fernandez <54313265+BrandonFernandez1@users.noreply.github.com>
2024-04-03 08:52:13 +0800

    Finished 03 reverse string

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,13 @@
-const reverseString = function() {
+const reverseString = function(string) {
+    const length = string.length;
+    let counter = 0;
+    let reverse = "";
 
+    while (counter < length) {
+        reverse = reverse + string[length - counter - 1];
+        counter++;
+    }
+    return reverse;
 };
 
 // Do not edit below this line
