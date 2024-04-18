2357de4a8670e91fd992faa56dda12b9706974a9
Brandon Fernandez <54313265+BrandonFernandez1@users.noreply.github.com>
2024-04-18 10:03:41 +0800

    Completed 09 palindrome

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,16 @@
-const palindromes = function () {
+const palindromes = function (str) {
+    const alphaNum = 'abcdefghijklmnopqrstuvwxyz0123456789';
 
+    const newStr = str
+        .toLowerCase()
+        .split('')
+        .filter((character) => alphaNum.includes(character))
+        .join('');
+    
+    const reverseStr = newStr.split('').reverse().join('');
+
+    if (newStr === reverseStr) return true;
+    else return false;
 };
 
 // Do not edit below this line
