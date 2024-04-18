a861f3edde6557b7c7d2036f9e7f33eb88d33c25
Brandon Fernandez <54313265+BrandonFernandez1@users.noreply.github.com>
2024-04-18 10:53:28 +0800

    Completed 10 fibonacci

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,6 +1,14 @@
-const fibonacci = function() {
+const fibonacci = function (index) {
+    if (index < 0) return 'OOPS';
 
-};
+    let numbers = [0, 1];
+
+    for (let i = 2; i <= index; i++) {
+        numbers[i] = numbers[i - 1] + numbers[i - 2];
+    }
+
+    return numbers[index];
+}
 
 // Do not edit below this line
 module.exports = fibonacci;
